0dedc4593bd7f1490ee347d471e035fc89a72b53
h3rt <94856309+SecretSaturn@users.noreply.github.com>
2022-11-13 01:14:49 +0100

    Improve UX & UI

--- frontend/src/components/FaucetHeader.vue
+++ frontend/src/components/FaucetHeader.vue
@@ -2,23 +2,33 @@
 .section-header
   h1 Secret Network Fee Grant Faucet
 
-  p Use this faucet to get a fee grant for your Secret Network wallet. 
+  p Use this faucet to get a fee grant for your Secret Network wallet.
   
-  p This is still in beta at the moments as we get wallets and workflow ready.
+  p A Fee Grant gives you enough SCRT (up to 0.1 SCRT) to pay for transactions to fund your wallet and can only be used for paying transaction fees. 
 
-  p A Fee Grant gives you enough SCRT (0.1 SCRT) to pay for transactions to fund your wallet and can only be used for paying transaction fees.
-
-  p It automatically expires after 1 day, after which you can get a new fee grant. 
+  p It automatically expires after 1 day, after which you can get a new fee grant.
   
-  p To use the fee grant, please add the address secret1tq6y8waegggp4fv2fcxk3zmpsmlfadyc7lsd69 to the "granter" flag in your Fina Cash wallet.
+  p To use the fee grant, please add the address secret1tq6y8waegggp4fv2fcxk3zmpsmlfadyc7lsd69 <button @click="copy">Copy</button> to the "granter" flag in your Fina Cash wallet.
+  notifications(:notifications='notifications' theme='cosmos')
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import Notifications from "@nylira/vue-notifications";
+
 export default {
   name: "faucet-header",
   computed: {
-    ...mapGetters(["config"])
+    ...mapGetters(["notifications","config"])
+  },
+  methods: {
+    copy() {
+        navigator.clipboard.writeText("secret1tq6y8waegggp4fv2fcxk3zmpsmlfadyc7lsd69");
+        this.$store.commit("notify", {
+            title: "Successfully Granted Fee",
+            body:  `Copied fee granter address secret1tq6y8waegggp4fv2fcxk3zmpsmlfadyc7lsd69 to clipboard`
+          });
+    }
   }
 };
 </script>
